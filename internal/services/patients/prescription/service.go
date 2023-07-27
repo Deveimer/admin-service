12eed77e0dc32f70bc1974473f0726c3c92c9ce0
Abhinav Singh <abhinav.singh@zopsmart.com>
2023-07-28 01:25:39 +0530

    added skeleton for service layer

--- internal/services/patients/prescription/service.go
+++ internal/services/patients/prescription/service.go
@@ -1,1 +1,79 @@
 package prescription
+
+import (
+	"github.com/Deveimer/goofy/pkg/goofy"
+	"main/internal/filters"
+	"main/internal/models"
+	"main/internal/stores"
+)
+
+type PrescriptionService struct {
+	store stores.Prescription
+}
+
+func New(store stores.Prescription) *PrescriptionService {
+	return &PrescriptionService{
+		store: store,
+	}
+}
+
+func (s *PrescriptionService) Get(ctx *goofy.Context, patientID, doctorID string) (*models.PatientPrescription, error) {
+	// todo validate doctor and patient
+	filter := validateAndGetFilter(patientID, doctorID)
+	prescription, err := s.store.Get(ctx, filter)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return prescription, nil
+}
+
+func (s *PrescriptionService) Update(ctx *goofy.Context, req *models.PatientPrescription, patientID, doctorID string) (*models.PatientPrescription, error) {
+	prescription, err := s.Get(ctx, patientID, doctorID)
+	if err != nil || prescription == nil {
+		return nil, err
+	}
+
+	filter := validateAndGetFilter(patientID, doctorID)
+	err = s.store.Update(ctx, req, filter)
+
+	if err != nil {
+		return nil, err
+	}
+
+	updatedPrescription, err := s.store.GetByID(ctx, prescription.ID)
+	if err != nil || updatedPrescription == nil {
+		return nil, err
+	}
+
+	return updatedPrescription, nil
+}
+
+func (s *PrescriptionService) Delete(ctx *goofy.Context, req *models.PatientPrescription, doctorID string) error {
+	prescription, err := s.Get(ctx, "", doctorID)
+	if err != nil || prescription == nil {
+		return err
+	}
+
+	err = s.store.Delete(ctx, doctorID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateAndGetFilter(patientID, doctorID string) *filters.Prescription {
+	filter := filters.Prescription{}
+	if patientID != "" {
+		filter.PatientID = patientID
+	}
+
+	if doctorID != "" {
+		filter.DoctorID = doctorID
+	}
+
+	return &filter
+}
--- internal/stores/interfaces.go
+++ internal/stores/interfaces.go
@@ -1,1 +1,15 @@
 package stores
+
+import (
+	"github.com/Deveimer/goofy/pkg/goofy"
+	"main/internal/filters"
+	"main/internal/models"
+)
+
+type Prescription interface {
+	GetByID(ctx *goofy.Context, prescriptionID string) (*models.PatientPrescription, error)
+	Get(ctx *goofy.Context, filter *filters.Prescription) (*models.PatientPrescription, error)
+	Create(ctx *goofy.Context, prescription *models.PatientPrescription) (int, error)
+	Update(ctx *goofy.Context, requestData *models.PatientPrescription, filter *filters.Prescription) error
+	Delete(ctx *goofy.Context, doctorID string) error
+}
